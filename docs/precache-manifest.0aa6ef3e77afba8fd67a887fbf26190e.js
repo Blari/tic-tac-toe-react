e1ec0b91433eba0e6990561ddb5764f1f65d6d62
Blari <Blarior@gmail.com>
2021-06-21 23:58:13 +0300

    add docs dist

---
+++ docs/precache-manifest.0aa6ef3e77afba8fd67a887fbf26190e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b59e9d25b6ea824bb4daf54a63a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c9f9cf816d15842c1b",
+    "url": "/static/css/main.8f02d165.chunk.css"
+  },
+  {
+    "revision": "f70fa8d1ba390e7e745b",
+    "url": "/static/js/2.3d65f68b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d65f68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c9f9cf816d15842c1b",
+    "url": "/static/js/main.dd907a1f.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
